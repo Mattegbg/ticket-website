daef733383a5327e8c0d7d60154c50f8ac4fd70e
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-02-11 17:11:07 +0100

    added backend

---
+++ backend/database/operations.js
@@ -0,0 +1,144 @@
+// läsa in databasen / biblioteket
+const nedb = require('nedb-promise');
+const { hashPassword } = require('../utils/bcrypt');
+//skapa en ny databas. Autoload true är för att den ska automatiskt uppdatera
+const database = new nedb ({ filename: 'accounts.db', autoload: true });
+//const database1 = new nedb ({ filename: 'tickets.db', autoload: true});
+
+const menu = {
+    "type": "event-menu", 
+    "menu": [
+        
+        {
+            "id":1,
+            "artist":"ACDC",
+            "date":"10 JAN",
+            "time":"22.00",
+            "price":"250 SEK",
+            "place":"ULLEVI"  
+
+        },
+        {
+            "id":2,
+            "artist":"ABBA",
+            "date":"25 AUG",
+            "time":"21.00",
+            "price":"300 SEK",
+            "place":"HEDEN"
+        },
+        {
+            "id":3,
+            "artist":"AKON",
+            "date":"1 JAN",
+            "time":"18.00",
+            "price":"260 SEK",
+            "place":"PARTILLE ARENA"
+
+        }
+    ]
+}
+
+//find funktion för att söka efter användarnamn i databasen
+async function getAccountByUsername(username){
+    const account = await database.find({ username: username });
+    return account;
+}
+
+
+//Skapa/spara konto i databasen (insert account i databasen)
+function saveAccount(account) {
+    database.insert(account);
+}
+
+//här lägger vi in eventet ovan i databasen
+function saveMenu(){
+    database.insert(menu);
+}
+
+function saveTicket(ticket){
+    database.insert(ticket);
+    console.log(ticket);
+}
+
+async function getMenu(){
+    const menu = await database.find({type: 'event-menu'});
+    return menu;
+}
+
+async function createAdmin(){
+    const account = {username: "admin", password: await hashPassword("admin1")}
+    saveAccount(account)
+} 
+
+
+//hämta ticket och skicka till server.js tex. 
+async function getTicket(){
+    let ticket = await database.find({}) //hämtar allt i databasen.
+    console.log(ticket);
+    let lastThing = ticket[ticket.length -1]
+    return lastThing;
+}
+
+
+
+//floor tar bort decimaltal. 
+async function createTicket(id){
+        const ticketNumber = Math.floor(Math.random()*10000) // skapa random siffra utan decimaltal
+        const ticket = {ticket: await hashPassword(String(ticketNumber)), eventId: id } // hasha/kryptera lösenordet
+        saveTicket(ticket) // spara i databasen
+        return ticketNumber;  
+     } 
+
+//exportera våra funktion så vi importera dom i server.js 
+module.exports = { getAccountByUsername, saveAccount, saveMenu, getMenu, saveTicket, createTicket, getTicket}
+
+
+
+
+// const event = {
+//     type: "event-menu", 
+//     "menu": [
+        
+//         {
+//             "id":1,
+//             "artist":"ACDC",
+//             "date":"10 JAN",
+//             "time":"22.00",
+//             "price":"250 SEK",
+//             "place":"ULLEVI"  
+
+//         },
+//         {
+//             "id":2,
+//             "artist":"ABBA",
+//             "date":"25 AUG",
+//             "time":"21.00",
+//             "price":"300 SEK",
+//             "place":"HEDEN"
+//         },
+//         {
+//             "id":3,
+//             "artist":"AKON",
+//             "date":"1 JAN",
+//             "time":"18.00",
+//             "price":"260 SEK",
+//             "place":"PARTILLE ARENA"
+
+//         }
+//     ]
+// }
+
+// function saveMenu() {
+//     database.insert(menu);
+// }
+
+// async function getMenu() {
+//     const menu = await database.find({
+//        type: "event-menu"
+//     });
+//    return menu;
+// }
+
+// module.exports = { saveMenu, getMenu}
+
+// //getTicketName("");
---
+++ backend/server.js
@@ -0,0 +1,168 @@
+const { request, response } = require('express');
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+
+//hämta den exporterade funktionen från Operations i database mappen. 
+const { getAccountByUsername, saveAccount, saveMenu, getMenu, saveTicket, createTicket, getTicket } = require('./database/operations');
+
+//hämta/importera funktion (kryptera lösenord) från utils, bcrypt.js
+const { hashPassword, comparePassword } = require('./utils/bcrypt');
+
+
+app.use(express.static('../frontend'));
+app.use(express.json());
+
+//saveMenu(); // spara våra events i databasen när servern startas
+
+app.get('/api/ticket', async (request, response) => {
+ const ticket = await getTicket();
+ response.json(ticket);
+})
+
+
+app.post('/api/auth/create',  async (request, response) => {
+  const credentials = request.body;
+  //{username: 'ada', password: 'pwd123}
+  const resObj = {
+    success: true,
+    usernameExists: false
+
+  }
+  //console.log(credentials);
+
+  //här får vi tbx array. Antingen innehåller den konto eller inget. verifierar om användarnamet existerar eller ej. 
+  const usernameExists = await getAccountByUsername(credentials.username);
+
+  // Skapa user. Om den får tillbaka större än 0 är det namnet upptaget om ej se nedan.
+  if(usernameExists.length > 0) {
+    resObj.usernameExists = true;
+    resObj.success = false;
+  }
+
+  //Skapa user. om användarnamn är ledigt från ovan (kryptera lösenord och spara? sa han på föreläsning)
+  if (resObj.usernameExists == false) {
+    const hashedPassword = await hashPassword(credentials.password); //skicka in lösenordet du valt (credentials) tbx får du krypterat lösenord.
+    credentials.password = hashedPassword; // ditt lösenord blir nu det krypterade lösenordet ist.
+    
+    saveAccount(credentials);
+  
+  }
+    
+    response.json(resObj);
+
+})
+
+app.post('/api/createTicket', async (request, response) => {
+  const event = request.body;
+  const eventId = event.id;
+  const ticket = await createTicket(eventId); //await = vänta in svar från async funtion som ger OK att fortsätta. 
+  
+  response.json(ticket)
+})
+
+
+app.get('/api/getevent', async (request, response) => {
+  const menu = await getMenu();
+  response.send(menu); 
+
+})
+
+
+//endpoint till loginsidan. 
+app.post('/api/auth/login', async (request, response) => {
+  const credentials = request.body;
+ // { username: 'ada', password: 'pwd123'}
+
+  const resObj = {
+    success: false,
+    token: ''
+  }
+
+
+  //verifiera om konto finns eller ej vid inloggning. Finns ej konto kommer en tom array tillbaka, ser ut så här: []
+  const account = await getAccountByUsername(credentials.username);
+  console.log(account);
+
+
+  //inte tom array, om vi hittar konto (> 0 = större än noll, dvs det existerar). börja med att jämföra lösenordet i Bcrypt
+  if (account.length > 0) {
+    const correctPassword = await comparePassword(credentials.password, account[0].password);
+    console.log(correctPassword);
+    if (correctPassword) {
+      resObj.success = true;
+
+      //vår token blir krypterad med vårt användarnamn som kopplad token till en användare
+      
+      const token = jwt.sign({ username: account[0].username}, 'a1b1c1', {
+        expiresIn: 600 //token (jwt) går ut om 10 min / 600 sekunder (värdet skrivs ut i sekunder)
+      });
+
+
+        
+        resObj.token = token; //skickar/lägger in vår token till response objekt (resObj) ovan
+
+    }
+  }
+
+  response.json(resObj); //skickas tillbaka här från resObj token ovan
+
+})
+
+
+
+
+//kanske lägga till en till endpoint än bara event. 
+app.get('/api/event/menu', async (request, response) => { 
+  const token = request.headers.authorization.replace('Bearer ', '');
+console.log(request);
+  const resObj = {
+    success: false
+    
+  }
+
+  try {
+    const data = jwt.verify(token, 'a1b1c1');
+    // const eventMenu = await getMenu();
+    
+
+    // resObj.menu = eventMenu;
+    resObj.success = true;
+  } 
+  catch ( error) {
+    resObj.errorMessage = 'Token invalid';
+  }
+
+  response.json(resObj);
+
+}); 
+
+
+
+
+
+//startar localhost server 5000
+app.listen(5000, () => {
+  console.log('server started on port 5000');
+});
+
+
+
+
+
+
+
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const {getMenu, saveMenu} = require("./database/ticketMenu");
+
+// const app = express();
+
+
+// app.use(express.static('view'));
+
+
+// app.listen(3000, function () {
+//     console.log("Server started on port 3000");
+//   });
